ddec5da97e2ea8ce6fa316ca98a81194383f3b6b
Dylan Andrade <andrademdylan@gmail.com>
2025-08-01 13:52:55 +0200

    Exercise 1.2

--- src/main/scala/actors/ActorsIntro.scala
+++ src/main/scala/actors/ActorsIntro.scala
@@ -19,7 +19,8 @@ object ActorsIntro {
 
     // part 3 communicate with the actor
     // Note: message should be the same type as simpleActorBehavior expects Behavior[String]
-    actorSystem ! "Hello, Actor!" // async
+//    actorSystem ! "Hello, Actor!" // async
+    actorSystem ! "sad" // async
 
     // part 4 shut down the actor system
     Thread.sleep(1000) // wait for the actor to process the message
@@ -47,13 +48,25 @@ object ActorsIntro {
 
   object Person {
     def happy(): Behavior[String] = Behaviors.receive { (context, message) =>
-      context.log.info(s"I've received '$message'. That's great!'")
-      Behaviors.same
+      message match {
+        case "sad" =>
+          context.log.info(s"I'm a happy person, but I've received '$message'. Let's be sad.")
+          sad()
+        case _ =>
+          context.log.info(s"I've received '$message'. That's great!'")
+          Behaviors.same
+      }
     }
 
     def sad(): Behavior[String] = Behaviors.receive { (context, message) =>
-      context.log.info(s"I've received '$message'. That's sucks!'")
-      Behaviors.same
+      message match {
+        case "happy" =>
+          context.log.info(s"I'm a sad person, but I've received '$message'. Let's be happy.")
+          happy()
+        case _ =>
+          context.log.info(s"I've received '$message'. That's sucks!'")
+          Behaviors.same
+      }
     }
 
     def apply(): Behavior[String] = {
@@ -62,7 +75,23 @@ object ActorsIntro {
     }
   }
 
+  def testPerson(): Unit = {
+    val actorSystem = ActorSystem(Person(), "PersonActorSystem")
+
+    // Send messages to the Person actor
+    actorSystem ! "I love blue"
+    actorSystem ! "sad"
+    actorSystem ! "I also love red"
+    actorSystem ! "happy"
+    actorSystem ! "happy"
+
+    Thread.sleep(1000) // wait for the actor to process the messages
+    // Shut down the actor system
+    actorSystem.terminate()
+  }
+
   def main(args: Array[String]): Unit = {
-    demoSimpleActor()
+//    demoSimpleActor()
+    testPerson()
   }
 }
